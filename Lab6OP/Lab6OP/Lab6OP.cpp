89d822a1564cc938c1b2ec43af21d38ccdda0719
Podgore <ivan2005podhornyi@gmail.com>
2023-05-26 11:31:54 +0300

    Lab6OP updated

--- Lab6OP/Lab6OP/Lab6OP.cpp
+++ Lab6OP/Lab6OP/Lab6OP.cpp
@@ -8,22 +8,46 @@ using namespace std;
 int main()
 {
     BinaryTree<int> tree;
-    tree.Push(50);
-    tree.Push(48);
-    tree.Push(49);
-    tree.Push(23);
-    tree.Push(60);
-    tree.Push(55);
-    
-    cout << "Tree contents: ";
-    for (auto it = tree.begin(); it != tree.end(); ++it) {
-        cout << *it << " ";
+    while (true) {
+        int choice = 0;
+        int value = 0;
+        cout << "What do you want to do?" << endl;
+        cout << "1 - Add new value" << endl;
+        cout << "2 - Get size of binary tree" << endl;
+        cout << "3 - Show tree content(using an iterator)" << endl;
+        cout << "4 - Clear tree" << endl;
+        cout << "5 - Exit" << endl;
+        cin >> choice;
+        switch (choice)
+        {
+        case 1: 
+            cout << "Type Value" << endl;
+            cin >> value;
+            tree.Push(value);
+            continue;
+        case 2:
+            cout << "Tree size: " << tree.GetSize() << endl;
+            continue;
+        case 3:
+            cout << "Tree contents: ";
+            for (auto it = tree.begin(); it != tree.end(); ++it) {
+                cout << *it << " ";
+            }
+            cout << endl;
+            continue;
+        case 4:
+            tree.Clear();
+            continue;
+        case 5:
+            return 0;
+        }
+
     }
-    cout << endl;
+    
+    
+    
+    
 
-    cout << "Tree size: " << tree.GetSize() << endl;
-
-    tree.Clear();
 
     return 0;
 }
